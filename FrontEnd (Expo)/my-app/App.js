813d9dd9d287c1e41b9cc658b773cf2c24f23583
Enmanuel Urena <enmanuelurenav@gmail.com>
2022-07-18 16:59:00 -0400

    Frontend fixed and styles applied

--- FrontEnd (Expo)/my-app/App.js
+++ FrontEnd (Expo)/my-app/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { ActivityIndicator, Flatlist, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default class App extends Component
 {
@@ -11,7 +11,7 @@ export default class App extends Component
       data: [],
       isLoading: true
     };
- 
+
 }
 
 async getProducts()
@@ -20,11 +20,11 @@ async getProducts()
     {
       const response = await fetch('https://localhost:7204/api/Products');
       const json = await response.json();
-      this.state({data: json})
+      this.state.data = json;
     }
     catch(error)
     {
-      console.log('Error API', error);
+      console.error('Error API', error);
     }
     finally
     {
@@ -41,19 +41,26 @@ async getProducts()
   {
     const {data, isLoading} = this.state;
 
-    return 
-    (
-      <View style={{flex:1, padding:25}}>
+    return (
+      <View style={{flex:1, padding:25, backgroundColor: '#a6edab'}}>
         {isLoading ? <ActivityIndicator/> : (
           <FlatList data={data}
           keyExtractor={({id}, index) => id}
           renderItem ={({item}) => (
-            <Text>{item.name} - {item.price}</Text>
+            <View style={{flex:1, marginBottom:  10, borderWidth: 2, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+              <Image source={{uri: 'https://random.imagecdn.app/70/70'}} style={{ width: 100, height: 100 }} />
+              <Text style={{fontSize: 18}}>{item.name} - {item.price}$ </Text>
+              <TouchableOpacity
+                onPress={() => alert('Prodcuct: ' + item.name + '\nPrice: ' + item.price)}
+                style={{backgroundColor: '#33b2e8', height: 100, width: 100, justifyContent: 'center', alignItems: 'center'}}>
+                <Text style={{ fontSize: 20, color: '#fff'}}>More Info</Text>
+              </TouchableOpacity>
+            </View>
           )}/>
         )}
       </View>
+      
     );
   }
-
+  
 }
-
