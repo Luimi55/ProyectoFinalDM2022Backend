420aa8a439db077d09c3f6776b30e75b2c85365b
Luis Miguel Rosa <Miguelrosa5@hotmail.es>
2022-07-18 13:43:30 -0400

    Feat: added migrations

---
+++ ProyectoFinalDM2022/Migrations/20220718174235_migrations.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoFinalDM2022.Migrations
+{
+    public partial class migrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Markets",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    rating = table.Column<decimal>(type: "decimal(2,1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Markets", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    price = table.Column<decimal>(type: "money", nullable: false),
+                    market_id = table.Column<int>(type: "int", nullable: false),
+                    image = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true),
+                    created_at = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.id);
+                    table.ForeignKey(
+                        name: "FK__Products__market__267ABA7A",
+                        column: x => x.market_id,
+                        principalTable: "Markets",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_market_id",
+                table: "Products",
+                column: "market_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Markets");
+        }
+    }
+}
